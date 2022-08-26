28d729847b22b9fa6c111d3e2110253f7d108461
gonzaloalonsopicco <gonzaloalonsopicco@gmail.com>
2022-08-25 22:17:24 -0300

    clase 4

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,31 @@
 import './ItemListContainer.css'
+import { useState } from 'react'
 
-export function ItemListContainer({texto}){
+export function ItemListContainer({stock, inicial, agregarCarrito}){
+
+    const[contador, setContador] = useState(inicial);
+
+
+    const sumar = ()=>{
+        if(contador < stock){
+            setContador(contador + 1)
+        }
+    }
+
+    const restar = ()=>{
+        if(contador > 1){
+            setContador(contador - 1)
+        }
+    }
 
     return(
-        <div>
-            <p className='texto'>{texto}</p>
+        <div className='divContador'>
+            <h3>productos agregados : {contador}</h3>
+            <div>
+                <button className='botonContador' onClick={restar}>eliminar</button>
+                <button className='botonContador' onClick={sumar}>agregar</button>
+            </div>
+            <button className='botonContador' onClick={()=>(agregarCarrito(contador))}>agregar al carrito</button>
         </div>
     )
 }
